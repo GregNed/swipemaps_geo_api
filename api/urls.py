3c958d64f079011e5009806794d89f9f3309352f
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-01 10:26:13 +0300

    Add point-to-road snapping
    In general, when a user specifies a point on a map either by clicking
    or by dragging a marker, it will be near the road, but not exactly on it.
    Often, it may land in courtyard, a pedestrian zone, etc, and then the
    routing engine may not be able to display a valid route.
    The snapping endpoint accepts the user-specified point and returns the
    closest point on the closest primary, secondary or tertriary road. For
    the exact definitions of these, see:
    https://wiki.openstreetmap.org/wiki/Key:highway
    
    endpoint: /snap
    method: GET
    query parameter: point <lat,lon>
    returns: <lon,lat>
    e.g. /snap/?point=55.752908,37.613011
    
    Search radius is hardcoded as 1km. Within the Moscow Region, this should
    sufficient, otherwise the user may have dropped the point erroneously.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from api.views import geocode, reverse_geocode, route, healthcheck
+from api.views import geocode, reverse_geocode, route, healthcheck, snap_to_road
 from django.urls import path
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('geocode/', geocode, name='geocode'),
     path('reverse/', reverse_geocode, name='reverse_geocode'),
     path('directions/', route, name='directions'),
+    path('snap/', snap_to_road, name='snap'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import json
+import psycopg2 as pg
 import openrouteservice as ors
 from api import functions
 from api.helpers import parse_coords
@@ -43,3 +44,31 @@ def route(request):
         features[i]['properties']['id'] = i
     result = {'type': 'FeatureCollection', 'features': features}
     return HttpResponse(json.dumps(result))
+
+
+def snap_to_road(request):
+    pt = parse_coords(request.GET.get('point'))
+    credentials = {
+        'host': 'pg',
+        'port': 5432,
+        'user': 'postgres',
+        # 'password': '',
+        'dbname': 'postgres'
+    }
+    with pg.connect(**credentials) as conn:
+        cur = conn.cursor()
+        cur.execute("""with
+            pt as (select st_setsrid(st_point(%s, %s), 4326)::geography as geog), 
+            road as (
+                select roads.geog, st_distance(pt.geog, roads.geog) as dist 
+                from roads join pt 
+                on st_dwithin(roads.geog, pt.geog, 1000)
+                order by dist 
+                limit 1
+            ) 
+            select st_asgeojson(st_closestpoint(road.geog::geometry, pt.geog::geometry)) 
+            from pt, road;
+        """, pt)
+        pt = cur.fetchone()[0]
+        coords = json.loads(pt)['coordinates']
+    return HttpResponse(coords)
