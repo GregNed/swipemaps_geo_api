a0badebe463a8a493c9d8a7041d6599d14036f2d
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-01 10:48:07 +0300

    Set debug to True only if DANGO_ENV env var = "dev"

--- dango/settings.py
+++ dango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^i#@ngg6bn^c5^dh@vcw0)rh)1^8#_&7a%=_9exx2dz)4k2+il'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DANGO_ENV') == 'dev'
 
 ALLOWED_HOSTS = [
     "89.108.70.197",
