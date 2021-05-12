9ae7c2b25aa43a359c1ceffa1b5999e72c2c4811
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-12 23:11:54 +0300

    Add forgotten slashes

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,12 +16,12 @@ def test_healthcheck(client):
 
 
 def test_directions(client):
-    res = client.get('/directions?positions=55.7109996,37.4362684;55.6023977,37.723043&profile=driving-car')
+    res = client.get('/directions/?positions=55.7109996,37.4362684;55.6023977,37.723043&profile=driving-car')
     features = res.get_json().get('features')
     assert len(features) == 3  # factor out to some config
 
 
 def test_directions_via(client):
-    res = client.get('/directions?55.7109996,37.4362684;55.6259813,37.4744228;55.6023977,37.723043&profile=driving-car')
+    res = client.get('/directions/?55.7109996,37.4362684;55.6259813,37.4744228;55.6023977,37.723043&profile=driving-car')
     features = res.get_json().get('features')
     assert len(features) == 1
