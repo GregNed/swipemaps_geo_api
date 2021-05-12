5b679a3b319830c08af49fbde9747f7cf7a3f210
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-12 23:48:07 +0300

    Fix directions tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,11 @@ import pytest
 from api.api import app
 
 
+POSITION_VEREYSKAYA = '55.7109996,37.4362684'
+POSITION_MKAD = '55.6259813,37.4744228'
+POSITION_YASENEVAYA = '55.6023977,37.723043'
+
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
@@ -16,12 +21,20 @@ def test_healthcheck(client):
 
 
 def test_directions(client):
-    res = client.get('/directions/?positions=55.7109996,37.4362684;55.6023977,37.723043&profile=driving-car')
+    query_params = {
+        'positions': ';'.join((POSITION_VEREYSKAYA, POSITION_YASENEVAYA)),
+        'profile': 'driving-car'
+    }
+    res = client.get('/directions/', query_string=query_params)
     features = res.get_json().get('features')
     assert len(features) == 3  # factor out to some config
 
 
 def test_directions_via(client):
-    res = client.get('/directions/?55.7109996,37.4362684;55.6259813,37.4744228;55.6023977,37.723043&profile=driving-car')
+    query_params = {
+        'positions': ';'.join((POSITION_VEREYSKAYA, POSITION_MKAD, POSITION_YASENEVAYA)),
+        'profile': 'driving-car'
+    }
+    res = client.get('/directions/', query_string=query_params)
     features = res.get_json().get('features')
     assert len(features) == 1
