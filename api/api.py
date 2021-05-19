b6084c79b584dd701eb5b00d33b9aa8c13f0cb85
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-19 15:06:37 +0300

    Make snapping to roads part of Directions endpoint

--- api/api.py
+++ api/api.py
@@ -1,10 +1,8 @@
-import json
-
 from flask import Flask, request, jsonify
 from openrouteservice.convert import decode_polyline
 
 from api import ors
-from api import postgis
+from api.postgis import snap_to_road
 from api.helpers import parse_position
 
 
@@ -21,7 +19,8 @@ def directions():
     profile = request.args.get('profile')
     positions = request.args.get('positions')
     positions_parsed = [parse_position(p) for p in positions.split(';')]
-    routes = ors.directions(positions_parsed, profile)
+    positions_snapped = [snap_to_road(p) for p in positions_parsed]
+    routes = ors.directions(positions_snapped, profile)
     geoms = [decode_polyline(route['geometry']) for route in routes]
     features = [{'type': 'Feature', 'geometry': geom} for geom in geoms]
     return jsonify({'type': 'FeatureCollection', 'features': features})
@@ -39,14 +38,6 @@ def reverse_geocode():
     position = parse_position(request.args.get('point'))
     result = ors.reverse_geocode(position)
     return jsonify(result)
-
-
-@app.route('/snap/', methods=['GET'])
-def snap_to_road():
-    position = parse_position(request.args.get('position'))
-    snapped = postgis.snap_to_road(position)
-    lon, lat = json.loads(snapped)['coordinates']
-    return f'{lon},{lat}'
 
 
 if __name__ == '__main__':
--- api/postgis.py
+++ api/postgis.py
@@ -1,4 +1,12 @@
+import json
 import psycopg2 as pg
+
+
+conn_info = {
+    'host': 'pg',
+    'user': 'postgres',
+    'password': 'postgres'
+}
 
 
 def snap_to_road(position):
@@ -6,8 +14,8 @@ def snap_to_road(position):
 
     Accepts a position, returns a Point object.
     """
-    # Wrap in try-except
-    with pg.connect("host=pg user=postgres password=postgres") as conn:
+    try:
+        conn = pg.connect(**conn_info)
         cur = conn.cursor()
         cur.execute("""with
                 pt as (select st_setsrid(st_point(%s, %s), 4326)::geography as geog), 
@@ -21,4 +29,10 @@ def snap_to_road(position):
                 select st_asgeojson(st_closestpoint(road.geog::geometry, pt.geog::geometry)) 
                 from pt, road;
             """, position)
-        return cur.fetchone()[0]
+        return json.loads(next(cur)[0])['coordinates']
+    except (StopIteration, pg.ProgrammingError):
+        # if no results (most likely bc point is too far from roads)
+        return position
+    finally:
+        conn.close()
+        cur.close()
