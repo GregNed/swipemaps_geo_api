62e44aa135dbe1e1a0aff31674045f21fe97b091
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-21 16:31:16 +0300

    Make standalone /snap route
    It used to be part of the directions endpoint. But it may be useful for the
    app to snap point first, then pass them into a directions request. This way,
    the snapped points can be displayed earlier, while the routing is still in process.

--- api/api.py
+++ api/api.py
@@ -16,24 +16,27 @@ def healthcheck():
     return jsonify({'status': 'OK'})
 
 
-@app.route('/directions/', methods=['GET'])
+@app.route('/snap/', methods=['GET'])
+def snap():
+    positions = request.args.get('positions')
+    positions_parsed = [parse_position(p) for p in positions.split(';')]
+    positions_snapped = [snap_to_road(p) for p in positions_parsed]
+    return jsonify(FeatureCollection([Feature(i, Point(p)) for i, p in enumerate(positions_snapped)]))
+
+
+@ app.route('/directions/', methods=['GET'])
 def directions():
     profile = request.args.get('profile')
     positions = request.args.get('positions')
     positions_parsed = [parse_position(p) for p in positions.split(';')]  # use GeoJSON utils.map?
-    positions_snapped = [snap_to_road(p) for p in positions_parsed]
-    # Get JSON objects with a 'geometry' attr that is a Google Encoded Polyline
-    routes = ors.directions(positions_snapped, profile)
-    # Decode Google Encoded Polylines into GeoJSON coordinate lists
+    routes = ors.directions(positions_parsed, profile)
     route_coords = [decode_polyline(route['geometry'])['coordinates'] for route in routes]
-    sec_to_last_indexes = [coords.index(list(map(lambda x: round(x, 5), positions_snapped[-2]))) for coords in route_coords]
+    sec_to_last_indexes = [coords.index(list(map(lambda x: round(x, 5), positions_parsed[-2]))) for coords in route_coords]
     route_last_part_coords = [coords[i:] for coords, i in zip(route_coords, sec_to_last_indexes)]
     handles = [get_midpoint(coords) for coords in route_last_part_coords]
-    # positions_snapped_fc = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': position}} for position in positions_snapped]}
     return jsonify({
         'routes': FeatureCollection([Feature(i, LineString(coords)) for i, coords in enumerate(route_coords)]),
         'handles': FeatureCollection([Feature(i, Point(handle)) for i, handle in enumerate(handles)]),
-        # 'snapped': positions_snapped_fc}
     })
 
 
