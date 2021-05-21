9383f2d45819a129b67e1d69ec9f455d8a4c5d2e
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-21 13:25:19 +0300

    Change get_midpoint signature
    get_midpoint() now accepts a coordinate sequence
    (GeoJSON coordinate list) instead of a Geometry object. The demand for
    this was the need for getting the second-to-last coordinate for handle
    producing

--- api/geom_ops.py
+++ api/geom_ops.py
@@ -1,8 +1,8 @@
-from shapely.geometry import shape
+from shapely.geometry import LineString
 
 
-def get_midpoint(linestring_geom):
+def get_midpoint(coords):
     """"""
-    ls = shape(linestring_geom)
+    ls = LineString(coords)
     midpoint = ls.interpolate(0.5, normalized=True)
     return midpoint.coords[0]  # first position (out of 1), not lon
