6e08029cc1df0f8135f618d15bd98585c297d80e
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-21 13:28:46 +0300

    Return "handles" along with route geom
    Now, the Directions endpoint return an object with two keys: 'routes' & 'handles'.
    Both are GeoJSON FeatureCollection(s), routes being LineString(s), handles - Point(s).
    
    Handles are the midpoints of the last part of routes (between the last
    the second-to-last points).

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+from geojson import Point, LineString, Feature, FeatureCollection
 from flask import Flask, request, jsonify
 from openrouteservice.convert import decode_polyline
 
@@ -19,24 +20,31 @@ def healthcheck():
 def directions():
     profile = request.args.get('profile')
     positions = request.args.get('positions')
-    positions_parsed = [parse_position(p) for p in positions.split(';')]
+    positions_parsed = [parse_position(p) for p in positions.split(';')]  # use GeoJSON utils.map?
     positions_snapped = [snap_to_road(p) for p in positions_parsed]
+    # Get JSON objects with a 'geometry' attr that is a Google Encoded Polyline
     routes = ors.directions(positions_snapped, profile)
-    geoms = [decode_polyline(route['geometry']) for route in routes]
-    midpoints = [get_midpoint(geom) for geom in geoms]
-    routes_fc = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': geom} for geom in geoms]}
-    midpoints_fc = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': midpoint}} for midpoint in midpoints]}
-    return jsonify({'routes': routes_fc, 'midpoints': midpoints_fc})
+    # Decode Google Encoded Polylines into GeoJSON coordinate lists
+    route_coords = [decode_polyline(route['geometry'])['coordinates'] for route in routes]
+    sec_to_last_indexes = [coords.index(positions_snapped[-2]) for coords in route_coords]
+    route_last_part_coords = [coords[i:] for coords, i in zip(route_coords, sec_to_last_indexes)]
+    handles = [get_midpoint(coords) for coords in route_last_part_coords]
+    # positions_snapped_fc = {'type': 'FeatureCollection', 'features': [{'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': position}} for position in positions_snapped]}
+    return jsonify({
+        'routes': FeatureCollection([Feature(i, LineString(coords)) for i, coords in enumerate(route_coords)]),
+        'handles': FeatureCollection([Feature(i, Point(handle)) for i, handle in enumerate(handles)]),
+        # 'snapped': positions_snapped_fc}
+    })
 
 
-@app.route('/geocode/', methods=['GET'])
+@ app.route('/geocode/', methods=['GET'])
 def geocode():
     text = request.args.get('text')
     result = ors.geocode(text)
     return jsonify(result)
 
 
-@app.route('/reverse/', methods=['GET'])
+@ app.route('/reverse/', methods=['GET'])
 def reverse_geocode():
     position = parse_position(request.args.get('point'))
     result = ors.reverse_geocode(position)
