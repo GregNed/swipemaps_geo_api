d0199d512e09b1e85edb94ca1718afdfc4d3e289
Grigory Nedaev <g.nedaev@geoalert.io>
2021-05-30 08:27:46 +0300

    Add user id to /directions query params & response
    GET /directions now accepts a query param 'user' which stands for the user id.
    The response contains the same attribute.

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 from geojson import Point, LineString, Feature, FeatureCollection
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 from . import ors
 from .geom_ops import get_midpoint
@@ -28,12 +30,16 @@ def snap():
 @app.route('/directions/', methods=['GET'])
 def directions():
     profile = request.args.get('profile')
+    user_id = request.args.get('user_id')
     positions = parse_positions(request.args.get('positions'))
     alternatives = len(positions) == 2
     routes = ors.directions(positions, profile, alternatives)
+    # SAVE TO DB IN A THREAD
+    # RETURNS IDS
     routes_last_parts = routes if alternatives else ors.directions(positions[-2:], profile)
     handles = [get_midpoint(route) for route in routes_last_parts]
     return jsonify({
+        'user_id': user_id,
         'routes': FeatureCollection([Feature(i, LineString(coords)) for i, coords in enumerate(routes, 1)]),
         'handles': FeatureCollection([Feature(i, Point(handle)) for i, handle in enumerate(handles, 1)])
     })
