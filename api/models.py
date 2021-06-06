60e4d3fcab5477cfb5d3f8dc6b21c34aa60a3293
Grigory Nedaev <g.nedaev@geoalert.io>
2021-06-06 21:33:24 +0300

    Use single Route table

--- api/models.py
+++ api/models.py
@@ -4,29 +4,14 @@ from geoalchemy2 import Geography
 from api import db
 
 
-class Start(db.Model):
-    """"""
-    user_id = db.Column(UUID(as_uuid=True), primary_key=True)
-    geom = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'))
-
-    def __repr__(self):
-        return f'<User {self.user_id} start>'
-
-
-class Finish(db.Model):
-    """"""
-    user_id = db.Column(UUID(as_uuid=True), primary_key=True)
-    geom = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'))
-
-    def __repr__(self):
-        return f'<User {self.user_id} finish>'
-
-
 class Route(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
+    """"""
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
     user_id = db.Column(UUID(as_uuid=True), nullable=False)
-    geom = db.Column(Geography('LineString', srid=4326, from_text='ST_GeomFromGeoJSON'))
+    trip_id = db.Column(UUID(as_uuid=True))
+    route = db.Column(Geography('LineString', srid=4326, from_text='ST_GeomFromGeoJSON'))
+    start = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'), nullable=False)
+    finish = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'), nullable=False)
 
     def __repr__(self):
         return f'<User {self.user_id} route>'
--- api/routes.py
+++ api/routes.py
@@ -1,13 +1,14 @@
-import random
 from uuid import uuid4
 from concurrent.futures import ThreadPoolExecutor
+
+import requests
 
 from geojson import Point, LineString, Feature, FeatureCollection
 from flask import request, jsonify, abort, Response
 from sqlalchemy import func
 
 from api import app, db, ors
-from api.models import Start, Finish, Route
+from api.models import Route
 from api.geom_ops import get_midpoint
 from api.postgis import snap_to_road
 from api.helpers import parse_positions
@@ -32,62 +33,63 @@ def directions():
     profile = request.json.get('profile')
     user_id = request.json.get('user_id')
     positions = parse_positions(request.json.get('positions'))
-    # Save start & finish points to DB
-    start = Start(
-        geom=str(Point(positions[0])),
-        user_id=user_id
-    )
-    finish = Finish(
-        geom=str(Point(positions[-1])),
-        user_id=user_id
-    )
-    db.session().add(start)
-    db.session().add(finish)
-    db.session.commit()
-    # If it's 1st routing request for user's session, - request alternatives
-    is_initial = len(positions) == 2
+    # If it's driver's 1st routing request, do with alternatives
+    is_initial = (profile == 'driving-car') and (len(positions) == 2)
     routes = ors.directions(positions, profile, is_initial)
     # Save routes to DB
-    for route in routes:
-        route = Route(
-            geom=str(LineString(route)),
-            user_id=user_id
-        )
-        db.session.add(route)
+    route_ids = [uuid4() for route in routes]
+    for index, route in enumerate(routes):
+        db.session.add(Route(
+            id=route_ids[index],
+            user_id=user_id,
+            route=str(LineString(route)),
+            start=str(Point(positions[0])),
+            finish=str(Point(positions[-1]))
+        ))
     db.session.commit()
     # Get handles
     routes_last_parts = routes if is_initial else ors.directions(positions[-2:], profile)
     handles = [get_midpoint(route) for route in routes_last_parts]
     return jsonify({
         'user_id': user_id,
-        'routes': FeatureCollection([Feature(i, LineString(coords)) for i, coords in enumerate(routes, 1)]),
-        'handles': FeatureCollection([Feature(i, Point(handle)) for i, handle in enumerate(handles, 1)])
+        'routes': FeatureCollection([Feature(i, LineString(route)) for i, route in zip(route_ids, routes)]),
+        'handles': FeatureCollection([Feature(i, Point(handle)) for i, handle in zip(route_ids, handles)])
     })
 
 
-@app.route('/users/<uuid:user_id>/routes/<int:route_id>', methods=['DELETE'])
-def delete_discarded_routes(user_id, route_id):
-    count_deleted = Route.query.filter(Route.user_id == user_id, Route.id != route_id).delete()
-    if count_deleted:
-        db.session.commit()
-        return str(user_id)
-    else:
-        abort(Response('The requested user and route combination was not found', 404))
+@app.route('/routes/<uuid:route_id>', methods=['PUT'])
+def delete_discarded_routes(route_id):
+    user_id = request.args.get('user_id')
+    trip_id = request.json.get('trip_id')
+    count_deleted = Route.query.filter(
+        Route.user_id == user_id,
+        Route.id != route_id,
+        Route.trip_id == None
+    ).delete()
+    db.session.commit()
+    if count_deleted == 0:
+        abort(Response('User and route combination not found', 404))
+    Route.query.get(route_id).trip_id = trip_id
+    db.session.commit()
+    return ''
 
 
-@app.route('/sortPassengers', methods=['GET'])
-def sort_passengers(user_id, route_id):
+@app.route('/candidates', methods=['GET'])
+def get_candidates():
     user_id = request.args.get('user_id')
     route_id = request.args.get('route_id')
-    user_start = Location.query.filter(Location.user_id == user_id, Location.kind == 'start').first_or_404()
-    user_finish = Location.query.filter(Location.user_id == user_id, Location.kind == 'finish').first_or_404()
-    candidates = Location.query.order_by('user_id')
-    return jsonify([{
-        'user_id': user_id,
-        'route_id': route_id,
-        'passengers': {
-            'id': uuid4(),
-        }} for _ in range(2, random.randint(3, 10))])
+    route = Route.query.get_or_404(route_id)
+    if route.route:
+        candidates = Route.query.filter(Route.route == None)
+    else:
+        candidates = Route.query.filter(Route.route != None)
+    # candidates.order_by(func.ST_DWithin())
+    # return jsonify([{
+    #     'user_id': user_id,
+    #     'route_id': route_id,
+    #     'passengers': {
+    #         'id': uuid4(),
+    #     }} for _ in range(2, random.randint(3, 10))])
 
 
 @app.route('/geocode/', methods=['GET'])
