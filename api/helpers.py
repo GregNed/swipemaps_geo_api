0955668fedf30a3cc0b46141d977521ef9742158
Grigory Nedaev <g.nedaev@geoalert.io>
2021-06-06 17:31:14 +0300

    Accept a latlon array, return an array too

--- api/helpers.py
+++ api/helpers.py
@@ -1,6 +1,5 @@
 
 
-def parse_positions(latlon_string):
+def parse_positions(positions):
     """"""
-    positions_latlon = [position.split(',') for position in latlon_string.split(';')]
-    return [[float(position[1]), float(position[0])] for position in positions_latlon]
+    return [[position[1], position[0]] for position in positions]
