2fca988fc28f418bec75ffb157e770b6cf2ea09f
Grigory Nedaev <g.nedaev@geoalert.io>
2021-06-08 17:22:01 +0300

    Make Trip ID unique

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class Route(db.Model):
     """"""
     id = db.Column(UUID(as_uuid=True), primary_key=True)
     user_id = db.Column(UUID(as_uuid=True), nullable=False)
-    trip_id = db.Column(UUID(as_uuid=True))
+    trip_id = db.Column(UUID(as_uuid=True), unique=True)
     route = db.Column(Geography('LineString', srid=4326, from_text='ST_GeomFromGeoJSON'))
     start = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'), nullable=False)
     finish = db.Column(Geography('Point', srid=4326, from_text='ST_GeomFromGeoJSON'), nullable=False)
