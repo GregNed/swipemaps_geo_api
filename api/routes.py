bf0bf3955c4e9033a4628e01c19ef0592196a10d
Grigory Nedaev <g.nedaev@geoalert.io>
2021-06-12 13:34:54 +0300

    Always build pedestrian routes

--- api/routes.py
+++ api/routes.py
@@ -6,6 +6,7 @@ import shapely.geometry
 from geojson import Point, LineString, Feature, FeatureCollection
 from flask import request, jsonify, abort, Response
 from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
 from geoalchemy2.shape import to_shape
 
 from api import app, db, ors
@@ -19,28 +20,22 @@ def healthcheck():
     return jsonify({'status': 'OK'})
 
 
-@app.route('/snap/', methods=['GET'])
-def snap():
-    positions = parse_positions(request.json.get('positions'))
-    with ThreadPoolExecutor() as executor:
-        snapped = executor.map(snap_to_road, positions)
-    return jsonify(FeatureCollection([Feature(i, Point(p)) for i, p in enumerate(snapped, 1)]))
-
-
 @app.route('/directions/', methods=['POST'])
 def directions():
-    # Validate the profile param
+    # Parse the request
     available_profiles = ('driving-car', 'foot-walking')
     profile = request.json.get('profile')
     if profile not in available_profiles:
-        abort(Response(f'profile must be one of {available_profiles}', 400))
+        abort(Response(f'Profile must be one of {available_profiles}', 400))
     user_id = request.json.get('user_id')
     positions = parse_positions(request.json.get('positions'))
+    make_route = request.json.get('make_route', True)
     start = Point(positions[0])
     finish = Point(positions[-1])
-    if profile == 'driving-car':
+    handles = []
+    if make_route:
         # If it's driver's 1st routing request, do with alternatives
-        is_initial = len(positions) == 2
+        is_initial = (profile == 'driving-car' and len(positions) == 2)
         routes = ors.directions(positions, profile, is_initial)
         # Save routes to DB
         route_ids = [uuid4() for route in routes]
@@ -55,11 +50,13 @@ def directions():
                 duration=route['duration']
             ))
         # Get midpoints of the route's last segment for the user to drag on the screen
-        routes_last_parts = routes if is_initial else ors.directions(positions[-2:], profile)
-        routes_last_parts = [route['geometry'] for route in routes_last_parts]
-        handles = [shapely.geometry.LineString(route).interpolate(0.5, normalized=True) for route in routes_last_parts]
-        handles = [Point(handle.coords[0]) for handle in handles]
-        # Format routes & handles for the response
+        if profile == 'driving-car':
+            routes_last_parts = routes if is_initial else ors.directions(positions[-2:], profile)
+            routes_last_parts = [route['geometry'] for route in routes_last_parts]
+            handles = [shapely.geometry.LineString(route).interpolate(0.5, normalized=True) for route in routes_last_parts]
+            handles = [Point(handle.coords[0]) for handle in handles]
+            handles = FeatureCollection([Feature(route_id, handle) for route_id, handle in zip(route_ids, handles)])
+        # Prepare the response
         routes = FeatureCollection([
             Feature(
                 id=route_id,
@@ -69,14 +66,12 @@ def directions():
                     'duration': route['duration']
                 })
             for route_id, route in zip(route_ids, routes)])
-        handles = FeatureCollection([Feature(route_id, handle) for route_id, handle in zip(route_ids, handles)])
-    elif profile == 'foot-walking':
+    else:
         route_id = uuid4()
         db.session.add(Route(id=route_id, user_id=user_id, start=str(start), finish=str(finish)))
-        routes = FeatureCollection([Feature(route_id, LineString([start, finish]))])
-        handles = []
+        routes = FeatureCollection([Feature(id=route_id, geometry=LineString([start, finish]))])
     db.session.commit()
-    return jsonify({'user_id': user_id, 'routes': routes, 'handles': handles})
+    return jsonify({'routes': routes, 'handles': handles})
 
 
 @app.route('/routes/<uuid:route_id>', methods=['GET'])
@@ -89,10 +84,11 @@ def get_route(route_id):
             shapely_geojson.dumps(shapely_geojson.Feature(to_shape(route.start))),
             shapely_geojson.dumps(shapely_geojson.Feature(to_shape(route.finish)))
         ]
-    return response
+    # return jsonify({'r': response)
+    return ''
 
 
-@ app.route('/routes/<uuid:route_id>', methods=['PUT'])
+@app.route('/routes/<uuid:route_id>', methods=['PUT'])
 def delete_discarded_routes(route_id):
     user_id = request.args.get('user_id')
     trip_id = request.json.get('trip_id')
@@ -105,11 +101,14 @@ def delete_discarded_routes(route_id):
         if count_deleted == 0:
             abort(Response('User and route combination not found', 404))
         Route.query.get_or_404(route_id).trip_id = trip_id
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return Response('Such trip id already exists in the database', 400)
     except Exception as e:
         db.session.rollback()
-        return e
+        abort(400)
     else:
-        db.session.commit()
         return ''
 
 
@@ -123,14 +122,22 @@ def get_candidates():
     return jsonify(candidate_ids)
 
 
-@ app.route('/geocode/', methods=['GET'])
+@app.route('/geocode/', methods=['GET'])
 def geocode():
     text = request.args.get('text')
     result = ors.geocode(text)
     return jsonify(result)
 
 
-@ app.route('/reverse/', methods=['GET'])
+@app.route('/reverse/', methods=['GET'])
 def reverse_geocode():
     position = parse_positions(request.args.get('position'))[0]
     return jsonify(ors.reverse_geocode(position))
+
+
+@app.route('/snap/', methods=['GET'])
+def snap():
+    positions = parse_positions(request.json.get('positions'))
+    with ThreadPoolExecutor() as executor:
+        snapped = executor.map(snap_to_road, positions)
+    return jsonify(FeatureCollection([Feature(i, Point(p)) for i, p in enumerate(snapped, 1)]))
