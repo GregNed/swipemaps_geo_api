2837acd4735bdcbb1ecaddf18bfafb5332a50da7
Grigory Nedaev <g.nedaev@geoalert.io>
2021-08-17 15:03:19 +0300

    Fix merge issues (prev commit)
    - Move requirements.txt back to the root for the CI to work
    - Merge db migration branches

---
+++ migrations/versions/a96cb642cac7_merge_after_connexion_integration.py
@@ -0,0 +1,25 @@
+"""Merge after Connexion integration
+
+Revision ID: a96cb642cac7
+Revises: 8ab5a0d208c5, c20b12b4e3da
+Create Date: 2021-08-17 11:56:07.437722
+
+"""
+import geoalchemy2
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'a96cb642cac7'
+down_revision = ('8ab5a0d208c5', 'c20b12b4e3da')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
