65b22b60462ac547734245cef0fd4ed9828818ae
Grigory Nedaev <g.nedaev@geoalert.io>
2021-09-13 18:45:34 +0300

    Do not drop indexes

--- migrations/versions/6260dbb295fd_store_data_in_utm.py
+++ migrations/versions/6260dbb295fd_store_data_in_utm.py
@@ -18,23 +18,17 @@ depends_on = None
 
 def upgrade():
     op.add_column('dropoff_point', sa.Column('geom', geoalchemy2.types.Geometry(geometry_type='POINT', srid=32637, from_text='ST_GeomFromEWKT', name='geometry'), nullable=False))
-    op.drop_index('idx_dropoff_point_geog', table_name='dropoff_point')
     op.drop_column('dropoff_point', 'geog')
     op.add_column('pickup_point', sa.Column('geom', geoalchemy2.types.Geometry(geometry_type='POINT', srid=32637, from_text='ST_GeomFromEWKT', name='geometry'), nullable=False))
-    op.drop_index('idx_pickup_point_geog', table_name='pickup_point')
     op.drop_column('pickup_point', 'geog')
     op.add_column('route', sa.Column('geom', geoalchemy2.types.Geometry(geometry_type='LINESTRING', srid=32637, from_text='ST_GeomFromEWKT', name='geometry'), nullable=True))
-    op.drop_index('idx_route_geog', table_name='route')
     op.drop_column('route', 'geog')
 
 
 def downgrade():
     op.add_column('route', sa.Column('geog', geoalchemy2.types.Geography(geometry_type='LINESTRING', srid=4326, from_text='ST_GeogFromText', name='geography'), autoincrement=False, nullable=True))
-    op.create_index('idx_route_geog', 'route', ['geog'], unique=False)
     op.drop_column('route', 'geom')
     op.add_column('pickup_point', sa.Column('geog', geoalchemy2.types.Geography(geometry_type='POINT', srid=4326, from_text='ST_GeogFromText', name='geography'), autoincrement=False, nullable=False))
-    op.create_index('idx_pickup_point_geog', 'pickup_point', ['geog'], unique=False)
     op.drop_column('pickup_point', 'geom')
     op.add_column('dropoff_point', sa.Column('geog', geoalchemy2.types.Geography(geometry_type='POINT', srid=4326, from_text='ST_GeogFromText', name='geography'), autoincrement=False, nullable=False))
-    op.create_index('idx_dropoff_point_geog', 'dropoff_point', ['geog'], unique=False)
     op.drop_column('dropoff_point', 'geom')
